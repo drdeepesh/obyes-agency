d10fe22fe5d309253d94730639296bf6f1f5a364
drdeepesh <deepeshy023@gmail.com>
2024-07-29 15:55:46 +0530

    made loader successfully

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+
+let tl = gsap.timeline()
+
+tl.from('.line h1', {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5
+})
+
+tl.from('#line1-part1', {
+    opacity: 0,
+    onStart: function() {
+        let h5timer = document.querySelector('#line1-part1 h5');
+        let grow = 0;
+        let loaderInterval = setInterval(() => {
+            if(grow<100){
+                h5timer.innerHTML = grow++;
+            }
+            else{
+                h5timer.innerHTML = grow;
+                clearInterval(loaderInterval);
+            }
+        }, 35);
+    }
+})
+
+tl.to('.line h2', {
+    animationName: 'anime',
+    opacity:1
+})
+
+tl.to('#loader', {
+    opacity: 0,
+    duration: 0.2,
+    delay: 4
+})
+
+tl.from('#page1', {
+    opacity: 0,
+    delay: 0.2,
+    y: 1500,
+    duration: 0.5,
+    ease: Power4
+})
+
+tl.to('#loader', {
+    display: 'none'
+})
